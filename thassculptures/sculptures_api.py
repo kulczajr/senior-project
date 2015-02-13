8b8d9f8de40ce5bac6a8c97847c2c7e632ded9d8
songm <songm@rose-hulman.edu>
2015-02-12 20:51:41 -0500

    finished sculptures_api

--- thassculptures/sculptures_api.py
+++ thassculptures/sculptures_api.py
@@ -34,6 +34,50 @@ class SculptureApi(protorpc.remote.Service):
             
         return Sculpture(quote="deleted")
 
-
+    @Artist.method(name="artist.insert", path="artist/insert", http_method="POST")
+    def artist_insert(self, request):
+        if request.from_datastore:
+            my_quote = request
+        else:
+            my_quote = Artist(fname=request.fname, lname=request.lname, website_url=request.website_url, description=request.description)
+            
+        my_quote.put()
+        return my_quote
+    
+    @Artist.query_method(name="artist.list", path="artist/list", http_method="GET", query_fields=("limit", "order", "pageToken"))
+    def artist_list(self, query):
+        return query
+    
+    @Artist.method(name="artist.delete", path="artist/delete/{entityKey}", http_method="DELETE", request_fields=("entityKey",))
+    def artist_delete(self, request):
+        if not request.from_datastore:
+            raise endpoints.NotFoundException("KEY NOT FOUND")
+        else:
+            request.key.delete()
+            
+        return Artist(quote="deleted")
+    
+    @Comment.method(name="comment.insert", path="comment/insert", http_method="POST")
+    def comment_insert(self, request):
+        if request.from_datastore:
+            my_quote = request
+        else:
+            my_quote = Comment(sculpture_key=request.sculpture_key, author=request.author, content=request.content)
+            
+        my_quote.put()
+        return my_quote
+    
+    @Comment.query_method(name="comment.list", path="comment/list", http_method="GET", query_fields=("limit", "order", "pageToken"))
+    def comment_list(self, query):
+        return query
+    
+    @Comment.method(name="comment.delete", path="comment/delete/{entityKey}", http_method="DELETE", request_fields=("entityKey",))
+    def comment_delete(self, request):
+        if not request.from_datastore:
+            raise endpoints.NotFoundException("KEY NOT FOUND")
+        else:
+            request.key.delete()
+            
+        return Comment(quote="deleted")
 
 app = endpoints.api_server([SculptureApi], restricted=False)

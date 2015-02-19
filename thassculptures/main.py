2950a43c8c4f0f5eda4c66b9db1a24e96781a338
songm <songm@rose-hulman.edu>
2015-02-19 18:32:41 -0500

    adds artists now from addArtistPageHandler

--- thassculptures/main.py
+++ thassculptures/main.py
@@ -54,7 +54,7 @@ class MobileTestHandler(webapp2.RequestHandler):
             'title':"Flame of the Millennium",
             'artist':"Leonard Nierman",
             'description': "Flame of the Millennium by Leonardo Nierman, located on the campus of Rose-Hulman Institute of Technology, was the first sculpture in the Art Spaces collection. Its impressive form gives over to large gleaming surfaces which mirror the reflecting pool surrounding it, and offer a spectacle of changing colors and patterns for motorists along Historic National Road U.S. 40 as it enters Terre Haute. Artist Leonardo Nierman lives in Mexico City and works in a variety of media including paint, stained glass and tapestry, but most of his large scale outdoor works are stainless steel, as is the Flame. His education in physics and mathematics and his study of the psychology of color and music have helped to shape his artistic style. What is common to all of his works is the lively and uplifting spirit that inhabits his forms and the vibrancy of the material as it moves skyward.",
-            'image':"Some pretty picture", #not quite sure how to handle images with Jinja yet... will look in to
+            'image':"Some pretty picture",  # not quite sure how to handle images with Jinja yet... will look in to
             'audio': "ToDo",
             'location':"GeoPage"
         }
@@ -67,7 +67,7 @@ class fourHundredChar(webapp2.RequestHandler):
             'title':"Flame of the Millennium",
             'artist':"Leonard Nierman",
             'description': "Artist Leonardo Nierman lives in Mexico City and works in a variety of media including paint, stained glass and tapestry, but most of his large scale outdoor works are stainless steel, as is the Flame. His education in physics and mathematics and his study of the psychology of color and music have helped to shape his artistic style. What is common to all of his works is the lively and uplifting spirit that inhabits his forms and the vibrancy of the material as it moves skywar",
-            'image':"Some pretty picture", #not quite sure how to handle images with Jinja yet... will look in to
+            'image':"Some pretty picture",  # not quite sure how to handle images with Jinja yet... will look in to
             'audio': "ToDo",
             'location':"GeoPage"
         }
@@ -80,7 +80,7 @@ class eightHundredChar(webapp2.RequestHandler):
             'title':"Flame of the Millennium",
             'artist':"Leonard Nierman",
             'description': "Flame of the Millennium by Leonardo Nierman, located on the campus of Rose-Hulman Institute of Technology, was the first sculpture in the Art Spaces collection. Its impressive form gives over to large gleaming surfaces which mirror the reflecting pool surrounding it, and offer a spectacle of changing colors and patterns for motorists along Historic National Road U.S. 40 as it enters Terre Haute. Artist Leonardo Nierman lives in Mexico City and works in a variety of media including paint, stained glass and tapestry, but most of his large scale outdoor works are stainless steel, as is the Flame. His education in physics and mathematics and his study of the psychology of color and music have helped to shape his artistic style. What is common to all of his works is the lively and uplifting.",
-            'image':"Some pretty picture", #not quite sure how to handle images with Jinja yet... will look in to
+            'image':"Some pretty picture",  # not quite sure how to handle images with Jinja yet... will look in to
             'audio': "ToDo",
             'location':"GeoPage"
         }
@@ -93,7 +93,7 @@ class twelveHundredChar(webapp2.RequestHandler):
             'title':"Flame of the Millennium",
             'artist':"Leonard Nierman",
             'description': "Flame of the Millennium by Leonardo Nierman, located on the campus of Rose-Hulman Institute of Technology, was the first sculpture in the Art Spaces collection. Its impressive form gives over to large gleaming surfaces which mirror the reflecting pool surrounding it, and offer a spectacle of changing colors and patterns for motorists along Historic National Road U.S. 40 as it enters Terre Haute. Artist Leonardo Nierman lives in Mexico City and works in a variety of media including paint, stained glass and tapestry, but most of his large scale outdoor works are stainless steel, as is the Flame. His education in physics and mathematics and his study of the psychology of color and music have helped to shape his artistic style. What is common to all of his works is the lively and uplifting spFlame of the Millennium by Leonardo Nierman, located on the campus of Rose-Hulman Institute of TechnFlame of the Millennium by Leonardo Nierman, located on the campus of Rose-Hulman Institute of TechnFlame of the Millennium by Leonardo Nierman, located on the campus of Rose-Hulman Institute of TechnFlame of the Millennium by Leonardo Nierman, located on the campus of Rose-Hulman Institute of Techn.",
-            'image':"Some pretty picture", #not quite sure how to handle images with Jinja yet... will look in to
+            'image':"Some pretty picture",  # not quite sure how to handle images with Jinja yet... will look in to
             'audio': "ToDo",
             'location':"GeoPage"
         }
@@ -105,7 +105,7 @@ class SculpturesHandler(webapp2.RequestHandler):
         
         # Fetch all greetings and print them out.
         response = service.sculpture().list().execute()
-        #sculptures_query = Sculpture.query(ancestor=SCULPTURE_KEY)
+        # sculptures_query = Sculpture.query(ancestor=SCULPTURE_KEY)
         self.response.write(template.render({'response': response['items']}))
 class SculptureCardHandler(webapp2.RequestHandler):
     def post(self):
@@ -129,29 +129,29 @@ class ArtistsHandler(webapp2.RequestHandler):
         self.response.write(template.render())
 class AddSculptureHandler(webapp2.RequestHandler):
     def post(self):
-        #The non-string attributes probably are going to need changing here,
-        #but I don't know how to handle them yet.
+        # The non-string attributes probably are going to need changing here,
+        # but I don't know how to handle them yet.
         
-        #Also, a general gist of what's happening here:
-        #We check to see if the statue already exists.
-        #If it does, we're updating it.
-        #If it doesn't, we're creating a new one and adding it.
-        #It checks the request for an entity key, which is what
-        #it would contain if the sculpture exists.
-        new_sculpture = Sculpture(title = self.request.get("title"),
-                                  artist = None,
-                                  location = None,
-                                  description = self.request.get("description"),
-                                  image = None)
+        # Also, a general gist of what's happening here:
+        # We check to see if the statue already exists.
+        # If it does, we're updating it.
+        # If it doesn't, we're creating a new one and adding it.
+        # It checks the request for an entity key, which is what
+        # it would contain if the sculpture exists.
+        new_sculpture = Sculpture(title=self.request.get("title"),
+                                  artist=None,
+                                  location=None,
+                                  description=self.request.get("description"),
+                                  image=None)
         new_sculpture.put()
         self.redirect(self.request.referer)
 
 class AddArtistHandler(webapp2.RequestHandler):
     def post(self):
-        new_artist = Artist(fname = self.request.get("fname"), 
-                               lname = self.request.get("lname"), 
-                               website_url = self.request.get("website_url"), 
-                               description = self.request.get("description"))
+        new_artist = Artist(fname=self.request.get("fname"),
+                               lname=self.request.get("lname"),
+                               website_url=self.request.get("website_url"),
+                               description=self.request.get("description"))
         new_artist.put()
         self.redirect(self.request.referer)
 
@@ -164,21 +164,28 @@ class AddCommentHandler(webapp2.RequestHandler):
             comment.content = self.request.get("content")
             comment.put()
         else:
-            new_comment = Comment(parent = COMMENT_KEY, 
-                                   author = self.request.get("author"), 
-                                   content = self.request.get("content"))
+            new_comment = Comment(parent=COMMENT_KEY,
+                                   author=self.request.get("author"),
+                                   content=self.request.get("content"))
             new_comment.put()
         self.redirect(self.request.referer)
 
 class AddArtistPageHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_env.get_template("web/AddArtist.html")
-        #new_artist = Artist(fname = "WHAT", 
+        # new_artist = Artist(fname = "WHAT", 
         #               lname = "IS", 
         #               website_url = "GOING", 
         #               description = "ON")
-        #service.artist().insert(new_artist)
+        # service.artist().insert(new_artist)
         self.response.write(template.render())
+    def post(self):
+        new_artist = Artist(fname=self.request.get("fname"),
+                               lname=self.request.get("lname"),
+                               website_url=self.request.get("website_url"),
+                               description=self.request.get("description"))
+        new_artist.put()
+        self.redirect(self.request.referer)        
 
 class AddSculpturePageHandler(webapp2.RequestHandler):
     def get(self):
